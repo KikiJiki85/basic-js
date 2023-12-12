803c58601b1414aad7a34e4b96ce2c7b26f4caab
Aleksey Vaskin <alzor85@gmail.com>
2023-12-12 12:41:18 +0700

    feat: finish two functions

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,10 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || !sampleActivity.trim() || Number.isNaN(+sampleActivity) || Number(sampleActivity) <= 0 || Number(sampleActivity) > MODERN_ACTIVITY) return false;
+  const rateConstant = 0.693 / HALF_LIFE_PERIOD;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / rateConstant);
 }
 
 module.exports = {
