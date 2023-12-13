e5da601dad9584a7c0fcb00540b4af625ac9dc97
Aleksey Vaskin <alzor85@gmail.com>
2023-12-13 17:01:52 +0700

    feat: add some more functions

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    let acc = 1;
+    for (let j = i + 1; j < str.length; j++) {
+      if (str[i] !== str[j]) break;
+      else if (str[i] === str[j]) acc++;
+    }
+    if (acc > 1 && !result.includes(str[i])) result += acc + str[i];
+    else if (str[i] === str[i - 1]) continue;
+    else result += str[i];
+  }
+  return result;
 }
 
 module.exports = {
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let acc = 0;
+  let nStr = String(n);
+
+  for (let i = 0; i < nStr.length; i++) {
+    acc += Number(nStr[i]);
+    if (i === nStr.length - 1 && String(acc).length > 1) {
+      i = -1;
+      nStr = String(acc);
+      acc = 0;
+    }
+  }
+  return acc;
 }
 
 module.exports = {
