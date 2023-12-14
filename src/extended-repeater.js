aec49f43fe06bc612abecfec012d4098047afe6b
Aleksey Vaskin <alzor85@gmail.com>
2023-12-14 17:44:32 +0700

    feat: add two more functions

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function repeater(str, options) {
+  let repeatTimes = options.repeatTimes ? options.repeatTimes : 1;
+  let sep = options.separator ? options.separator : '+';
+  let add = typeof options.addition !== 'undefined' ? options.addition : '';
+  let addRepeat = options.additionRepeatTimes ? options.additionRepeatTimes : 0;
+  let addSep = options.additionSeparator ? options.additionSeparator : '|';
+  let addPlusSep = !addSep || !addRepeat ? add : (add + addSep).repeat(addRepeat).slice(0, -addSep.length);
 
+  return (str + addPlusSep + sep).repeat(repeatTimes).slice(0, -sep.length);
+}
 module.exports = {
   repeater
 };
